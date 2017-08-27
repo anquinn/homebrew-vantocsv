f3bb267d43f4b9ea1d316bf3abf084f97fe74c69
Andrew Quinn <andrew.quinn2@gmail.com>
2017-08-27 18:50:01 -0400

    update sha256

--- vantocsv.rb
+++ vantocsv.rb
@@ -2,7 +2,7 @@ class Vantocsv < Formula
   desc "Converts VAN/liberalist PDF print files to CSV"
   homepage "https://github.com/anquinn/vantocsv"
   url "https://github.com/anquinn/vantocsv/archive/v1.0.0.tar.gz"
-  sha256 "8efe12b068ff6812331067cf88c912ae90bb49c3aac64d07203e13065c8e62a0"
+  sha256 "0c9a0d84faebbb521f715d883094570a110aa382db7ed4ef52203fb67977ba46"
 
   depends_on "poppler"
 
