a0a44d0920d65ed446f73806db0ab825ecbac318
Andrew Quinn <andrew.quinn2@gmail.com>
2017-08-29 11:05:35 -0400

    version bump to v1.0.1

--- vantocsv.rb
+++ vantocsv.rb
@@ -1,8 +1,8 @@
 class Vantocsv < Formula
   desc "Converts VAN/liberalist PDF print files to CSV"
   homepage "https://github.com/anquinn/vantocsv"
-  url "https://github.com/anquinn/vantocsv/archive/v1.0.0.tar.gz"
-  sha256 "0c9a0d84faebbb521f715d883094570a110aa382db7ed4ef52203fb67977ba46"
+  url "https://github.com/anquinn/vantocsv/archive/v1.0.1.tar.gz"
+  sha256 "2beaa7d371632d47a4fd8fb9c56715dbadaa727fc8bb8c0208033194aa13d16f"
 
   depends_on "poppler"
 
